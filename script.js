38893777016aa71bba078980ce77c62e393cf78f
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-04-20 19:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
+function Person(name,age) {
+	this._name = name;
+	this._age = age;
+
 }
 
 Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
 }
 
-function Employee(name, age, jobTitle) {
+function Employee(name,age,jobTitle) {
 	Person.call(this,name,age);
-	this.jobTitle = jobTitle;
+	this._jobTitle = jobTitle;
 }
+
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
- 
+
 Employee.prototype.jobGreet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	
+	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
 }
+
 // const p = new Person('Alice',25);
 // p.greet();
 // const e = new Employee('Bob',30,'Manager');
